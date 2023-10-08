3874ea7307889f58d301716cc95f7c86ac5852a6
RajivSonDev <professionalCoderHelloWorld@outlook.com>
2023-10-08 09:33:38 +0200

    Routing File is included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import {RouterLink, RouterModule, RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    SellerAuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterLink,
+    RouterModule,
+    RouterOutlet,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
