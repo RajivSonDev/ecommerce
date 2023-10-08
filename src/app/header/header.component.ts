0ea8a71306081e667e12dfb7c3a872056d0cb9a2
RajivSonDev <professionalCoderHelloWorld@outlook.com>
2023-10-08 17:41:03 +0200

    Navigation Update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,17 +8,27 @@ import {Router} from "@angular/router";
 })
 export class HeaderComponent implements OnInit{
 
+    menuType:string='default';
+
     isAuthenticated:boolean=false;
     constructor(private route:Router) {
     }
 
     ngOnInit() {
 
-        if(localStorage.getItem('seller')){
-            this.isAuthenticated=true;
-        }else{
-            this.isAuthenticated=false;
-        }
+        // whenever router change then you will receive value
+        this.route.events.subscribe((value:any)=>{
+            console.log(value.url)
+            if(value.url){
+                if(localStorage.getItem('seller') && value.url.includes('seller')){
+                    console.warn("in seller area")
+                    this.menuType="seller";
+                }else{
+                    console.warn("outside of seller area")
+                    this.menuType="default";
+                }
+            }
+        })
 
     }
 
